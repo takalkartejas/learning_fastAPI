f47cf982d6e51d86d7942910ac36d8e15932cd94
tt-213268 <takalkartejastt@gmail.com>
2022-12-21 19:43:11 +0100

    main.py:- added two users in database

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from typing import Optional, List
 from fastapi import FastAPI
+from uuid import uuid4, UUID
+#models is the file that we created and we import the classes in this program
+from models import User, Gender, Role
 
 #create and instance of application
 app = FastAPI()
 
+#create a list of users and add couple of users
+db: List[User] = [
+    User(
+     id=uuid4(),
+     first_name="Tejas",
+     last_name="Takalkar",
+     gender = Gender.male,
+     roles = [Role.student]
+    ),
+
+    User(
+     id=uuid4(),
+     first_name="Alexa",
+     last_name="Jones",
+     gender = Gender.female,
+     roles = [Role.admin, Role.user]
+    )    
+]
 #This will give a route for a get request, "/" :- indicates root
 @app.get("/")
 
